9ecf00846bd879a7774659e58b33abe23490a44a
AntonS <41778345+AntonS2@users.noreply.github.com>
2021-04-15 12:34:22 +0300

    цу

--- arduino1.php
+++ arduino1.php
@@ -21,43 +21,43 @@ function sendFile($path) {
     readfile($path);
 }
 
-if(!check_header('HTTP_USER_AGENT', 'ESP32-http-Update')) {
+if(!check_header('HTTP_USER_AGENT', 'ESP8266-http-Update')) {
     header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
-    echo "only for ESP32 updater!\n";
+    echo "only for ESP8266 updater!\n";
     exit();
 }
 
 if(
-    !check_header('x-ESP32-STA-MAC') ||
-    !check_header('x-ESP32-AP-MAC') ||
-    !check_header('x-ESP32-free-space') ||
-    !check_header('x-ESP32-sketch-size') ||
-    !check_header('x-ESP32-sketch-md5') ||
-    !check_header('x-ESP32-chip-size') ||
-    !check_header('x-ESP32-sdk-version')
+    !check_header('HTTP_X_ESP8266_STA_MAC') ||
+    !check_header('HTTP_X_ESP8266_AP_MAC') ||
+    !check_header('HTTP_X_ESP8266_FREE_SPACE') ||
+    !check_header('HTTP_X_ESP8266_SKETCH_SIZE') ||
+    !check_header('HTTP_X_ESP8266_SKETCH_MD5') ||
+    !check_header('HTTP_X_ESP8266_CHIP_SIZE') ||
+    !check_header('HTTP_X_ESP8266_SDK_VERSION')
 ) {
     header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
-    echo "only for ESP32 updater! (header)\n";
+    echo "only for ESP8266 updater! (header)\n";
     exit();
 }
 
  $db = array(
-    "7C:9E:BD:E3:14:24" => "v1"
+    "7c:9e:bd:e3:14:24" => "v1"
     
  );
 
- if(!isset($db[$_SERVER['x-ESP32-STA-MAC']])) {
+ if(!isset($db[$_SERVER['HTTP_X_ESP8266_STA_MAC']])) {
     header($_SERVER["SERVER_PROTOCOL"].' 500 ESP MAC not configured for updates', true, 500);
  }
 
- $localBinary = "./bin/".$db[$_SERVER['x-ESP32-STA-MAC']].".bin";
+ $localBinary = "./bin/".$db[$_SERVER['HTTP_X_ESP8266_STA_MAC']]."Blink1.bin";
 
 // проверяем, прислал ли ESP8266 версию прошивки;
 // если она не соответствует, проверяем соответствие MD5-хэшэй между 
 // бинарным файлом на сервере и бинарным файлом на ESP8266;
 // если они не соответствуют, то апдейта выполнено не будет:
-if((!check_header('x-ESP32-sdk-version') && $db[$_SERVER['x-ESP32-STA-MAC']] != $_SERVER['x-ESP32-sdk-version'])
-    || $_SERVER["x-ESP32-sketch-md5"] != md5_file($localBinary)) {
+if((!check_header('HTTP_X_ESP8266_SDK_VERSION') && $db[$_SERVER['HTTP_X_ESP8266_STA_MAC']] != $_SERVER['HTTP_X_ESP8266_VERSION'])
+    || $_SERVER["HTTP_X_ESP8266_SKETCH_MD5"] != md5_file($localBinary)) {
     sendFile($localBinary);
 } else {
     header($_SERVER["SERVER_PROTOCOL"].' 304 Not Modified', true, 304);
